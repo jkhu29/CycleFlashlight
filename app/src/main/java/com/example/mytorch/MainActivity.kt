4755b426e78a5f0e8844ae6ff63b36865368f640
jkhu29 <jiakuihu29@gmail.com>
2022-02-16 01:47:54 +0800

    first commit: add src

---
+++ app/src/main/java/com/example/mytorch/MainActivity.kt
@@ -0,0 +1,156 @@
+package com.example.mytorch
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.os.Bundle
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.concurrent.timerTask
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var cameraManager: CameraManager
+    private lateinit var cameraId: String
+    private lateinit var handler: Handler
+    private lateinit var timer: Timer
+
+    private var isTorchOn = false
+    private var isCycleOn = false
+    private var isCycleTimeMade = false
+    private var torchCyclePeriod = 2000L
+
+    private val torchCallback = object: CameraManager.TorchCallback() {
+        override fun onTorchModeUnavailable(camera_id: String) {
+            // super.onTorchModeUnavailable(camera_id)
+            if (camera_id == cameraId) {
+                Toast.makeText(this@MainActivity, "Torch is not available", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        override fun onTorchModeChanged(camera_id: String, camera_enabled: Boolean) {
+            // super.onTorchModeChanged(camera_id, camera_enabled)
+            if (camera_id == cameraId) {
+                isTorchOn = camera_enabled
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    override fun onDestroy() {
+        handler.removeCallbacksAndMessages(null)
+        cameraManager.unregisterTorchCallback(torchCallback)
+        super.onDestroy()
+    }
+
+    private fun init() {
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        handler = Handler()
+        cameraManager.registerTorchCallback(torchCallback, handler)
+
+        // editText, get the cycle time
+        // button, make sure the number is got
+        // textView, display the number
+        setCycleTime()
+
+        val cameraIds = getCameraIds()
+        if (cameraIds.isNullOrEmpty()) {
+            Toast.makeText(this, "Flashlight is not available, please check your CameraIds", Toast.LENGTH_SHORT).show()
+        } else {
+            cameraId = cameraIds[0]
+            btn_switch.setOnClickListener { toggleTorchCycle() }
+        }
+    }
+
+    private fun setCycleTime() {
+        editText.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                isCycleTimeMade = false
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                isCycleTimeMade = false
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                val content:String=p0.toString()
+                button.setOnClickListener {
+                    isCycleTimeMade = true
+                }
+                if (
+                    "" != content.trim { it <= ' ' }
+                    && content.trim { it <= ' ' }.isNotEmpty()
+                ) {
+                    torchCyclePeriod = content.toLong()
+                    displayCycleTime()
+                }
+            }
+        })
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun displayCycleTime() {
+        textView.text = "Your Cycle Time is :$torchCyclePeriod"
+    }
+
+    private fun toggleTorchCycle() {
+        isCycleOn = !isCycleOn
+        btn_switch.setBackgroundResource(
+            if (isCycleOn) {
+                R.drawable.torch_turn_on
+            } else {
+                R.drawable.torch_turn_off
+            }
+        )
+        try {
+            if (isCycleOn) {
+                timer = Timer()
+                val timerTask: TimerTask = object : TimerTask() {
+                    override fun run() {
+                        toggleTorch()
+                    }
+                }
+                timer.schedule(timerTask, 0, torchCyclePeriod)
+            }
+            else {
+                timer.cancel()
+                timer.purge()
+                if (isTorchOn)
+                    toggleTorch()
+            }
+        } catch (exception: CameraAccessException) {
+            Toast.makeText(this, "Cycle Switch is not OK", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun toggleTorch() {
+        try {
+            cameraManager.setTorchMode(cameraId, !isTorchOn)
+        } catch (exception: CameraAccessException) {
+            Toast.makeText(this, getToggleTorchErrorMessage(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getToggleTorchErrorMessage(): String {
+        return if (isTorchOn) "Cannot Turn Off" else "Cannot Turn On"
+    }
+
+    private fun getCameraIds(): Array<String> {
+        return try {
+            cameraManager.cameraIdList
+        } catch (exception: CameraAccessException) {
+            arrayOf()
+        }
+    }
+
+}
